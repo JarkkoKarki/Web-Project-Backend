6dbb6648d438aa89b91ca112e9679fef970c9a7a
JarkkoKarki <karkijarkko@gmail.com>
2025-04-21 14:04:42 +0300

    feat: set up Express server with basic routing and static file serving

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Toimii");
+import express from "express";
+const hostname = "127.0.0.1";
+const app = express();
+const port = 3000;
+
+app.use("/public", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Project REST API!");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
