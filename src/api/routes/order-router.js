b01fdf67bca43a16c387fd5fbb6a09f250755c28
jonijermu <joni.heikkila@metropolia.fi>
2025-04-30 20:36:05 +0300

    feat: add authentication middleware to postOrder route for enhanced security

--- src/api/routes/order-router.js
+++ src/api/routes/order-router.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {getOrders, postOrder} from "../controllers/order-controller.js";
+import {authenticateToken} from "../middlewares/middlewares.js";
 
 
 const orderRouter = express.Router();
 
-orderRouter.route("/").get(getOrders).post(postOrder);
+orderRouter.route("/").get(getOrders).post(authenticateToken, postOrder);
 
 orderRouter.route("/:id").delete().put().get();
 
