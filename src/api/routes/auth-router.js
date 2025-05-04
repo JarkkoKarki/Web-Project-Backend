ff034fddc216f670e9ccb38ecec88fd4ac5128ae
onnik <onni.kivinen@gmail.com>
2025-05-04 16:24:20 +0300

    fix: enhance /me route to retrieve user details and improve error handling

--- src/api/routes/auth-router.js
+++ src/api/routes/auth-router.js
@@ -1,14 +1,26 @@
 import { authUser, postUserAdmin } from "../controllers/auth-controller.js";
 import { authenticateToken, checkAdmin } from "../middlewares/middlewares.js";
 import express from "express";
+import { findUserById } from "../models/user-model.js";
 
 const authRouter = express.Router();
 
 authRouter.route("/login").post(authUser);
 
-authRouter.get("/me", authenticateToken, (req, res) => {
-  console.log(req);
-  res.json(res.locals.user);
+authRouter.get("/me", authenticateToken, async (req, res) => {
+  try {
+    const userId = res.locals.user.user_id;
+    const user = await findUserById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error("Error in /me route:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 authRouter.post("/register", authenticateToken, checkAdmin, postUserAdmin);
