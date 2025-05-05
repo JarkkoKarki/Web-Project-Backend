a5b77e29c0079703f641f2fce308c4f2bc978ccc
onnik <onni.kivinen@gmail.com>
2025-05-05 19:07:12 +0300

    fix: comment out logging of raw response text in fetchData function

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export async function fetchData(url, options = {}) {
   const response = await fetch(url, options);
   const responseText = await response.text();
-  console.log("Raw Response Text:", responseText);
+  // console.log("Raw Response Text:", responseText);
 
   const contentType = response.headers.get("content-type");
   if (!contentType || !contentType.includes("application/json")) {
