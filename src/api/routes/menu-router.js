1a3786b3aeaf18787990eee9a108b8def0625fe4
onnik <onni.kivinen@gmail.com>
2025-05-06 15:01:04 +0300

    feat: add createMenuThumbnail middleware for handling menu image uploads and thumbnail creation

--- src/api/routes/menu-router.js
+++ src/api/routes/menu-router.js
@@ -1,18 +1,22 @@
 import express from "express";
 import {
-    getProduct,
-    putProduct,
-    postProduct,
-    getProductById,
-    deleteProduct,
-    getProductsBothLanguages,
+  getProduct,
+  putProduct,
+  postProduct,
+  getProductById,
+  deleteProduct,
+  getProductsBothLanguages,
 } from "../controllers/menu-controller.js";
-import {checkAdmin, createThumbnail, upload} from "../middlewares/middlewares.js";
+import {
+  checkAdmin,
+  createMenuThumbnail,
+  upload,
+} from "../middlewares/middlewares.js";
 
 const menuRouter = express.Router();
 
 // GET /api/menu/ to get both languages fi and en
-menuRouter.route("/").get(getProductsBothLanguages)
+menuRouter.route("/").get(getProductsBothLanguages);
 
 // GET /api/menu/products/:lang
 menuRouter.route("/products/:lang").get(getProduct);
@@ -21,17 +25,16 @@ menuRouter.route("/products/:lang").get(getProduct);
 menuRouter.get("/products/:id/", getProductById);
 
 // POST /api/menu/products
-menuRouter.route("/").post(checkAdmin,
-    upload.single('file'),
-    createThumbnail,
-    postProduct);
+menuRouter
+  .route("/")
+  .post(checkAdmin, upload.single("file"), createMenuThumbnail, postProduct);
 
 // PUT /api/menu/products/:id
 // DELETE /api/menu/products/:id
-menuRouter.route("/:id").get(getProductById)
-    .delete(checkAdmin, deleteProduct)
-    .put(checkAdmin, upload.single('file'),
-        createThumbnail,
-        putProduct);
+menuRouter
+  .route("/:id")
+  .get(getProductById)
+  .delete(checkAdmin, deleteProduct)
+  .put(checkAdmin, upload.single("file"), createMenuThumbnail, putProduct);
 
 export default menuRouter;
