454e27c859806e8a09d188fa2f5613f73c3fcfb2
TonyKarlin <tony.karlin@metropolia.fi>
2025-05-06 14:38:42 +0300

    fix: update delete route to use correct deleteReservationByUserId function

--- src/api/routes/reservation-router.js
+++ src/api/routes/reservation-router.js
@@ -3,6 +3,7 @@ import {
   postReservation,
   getReservations,
   getReservationsByUserId,
+  deleteReservationByUserId,
 } from "../controllers/reservation-controller.js";
 import { authUser } from "../controllers/auth-controller.js";
 
@@ -15,6 +16,6 @@ reservationRouter.route("/").get(getReservations);
 reservationRouter
   .route("/:id")
   .get(getReservationsByUserId)
-  .delete(authUser, getReservationsByUserId);
+  .delete(authUser, deleteReservationByUserId);
 
 export default reservationRouter;
