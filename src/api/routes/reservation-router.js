290b64c352c6f821c0de9c5337fd3a1ee86247cf
onnik <onni.kivinen@gmail.com>
2025-05-06 15:41:35 +0300

    fix: update authentication middleware for delete reservation route

--- src/api/routes/reservation-router.js
+++ src/api/routes/reservation-router.js
@@ -5,18 +5,18 @@ import {
   getReservationsByUserId,
   deleteReservationById,
 } from "../controllers/reservation-controller.js";
-import { authUser } from "../controllers/auth-controller.js";
+import { authenticateToken } from "../middlewares/middlewares.js";
 
 const reservationRouter = express.Router();
 
 reservationRouter.route("/").get(getReservations);
 
--reservationRouter.route("/reserve").post(postReservation);
+reservationRouter.route("/reserve").post(postReservation);
 
 reservationRouter.route("/:id").get(getReservationsByUserId);
 
 reservationRouter
   .route("/:reservationId")
-  .delete(authUser, deleteReservationById);
+  .delete(authenticateToken, deleteReservationById);
 
 export default reservationRouter;
